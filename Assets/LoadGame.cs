4082520d985dd127a8f143f341323ae6415239c7
Jordan Marsh <jordanmarshdesign@gmail.com>
2019-09-13 08:31:59 -0700

    Initial Commit

---
+++ Assets/LoadGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+
+    private void Awake()
+    {
+        if (FindObjectsOfType<LoadGame>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(LoadNextScene());
+	}
+	
+	IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float xSpeed = 4f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float ySpeed = 4f;
+    [SerializeField] float yRange = 5f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalThrow = Mathf.Clamp((Input.GetAxis("Horizontal") * xSpeed * Time.deltaTime) + transform.localPosition.x,-xRange,xRange);
+        float verticalThrow = Mathf.Clamp((Input.GetAxis("Vertical") * ySpeed * Time.deltaTime) + transform.localPosition.y,-yRange,yRange);
+        transform.localPosition = new Vector3(horizontalThrow, verticalThrow, transform.localPosition.z);
+	}
+}
