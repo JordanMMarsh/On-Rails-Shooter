0947610ab6d1d71a83a13f8437bb5ab453953b21
Jordan Marsh <jordanmarshdesign@gmail.com>
2019-09-14 08:53:21 -0700

    Ship firing and collisions.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField] float xSpeed = 4f;
-    [SerializeField] float xRange = 5f;
-    [SerializeField] float ySpeed = 4f;
-    [SerializeField] float yRange = 5f;
-    
+    [SerializeField] float xSpeed = 10f;
+    [SerializeField] float xRange = 6f;
+    [SerializeField] float ySpeed = 10f;
+    [SerializeField] float yRange = 3f;
+
+    //Multiplied against the Y position to get rotation for ship
+    [SerializeField] float positionPitchFactor = -5f;
+
+    //Multiplied against vertical input to force nose of ship up or down while steering in that direction
+    [SerializeField] float controlPitchFactor = -1000f;
+
+    //Multiplied against X position of ship to get rotation for yaw
+    [SerializeField] float positionYawFactor = 5f;
+
+    //Multiplied against horizontal input to get ship to roll during movement
+    [SerializeField] float controlYawFactor = -50f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +27,29 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float horizontalThrow = Mathf.Clamp((Input.GetAxis("Horizontal") * xSpeed * Time.deltaTime) + transform.localPosition.x,-xRange,xRange);
-        float verticalThrow = Mathf.Clamp((Input.GetAxis("Vertical") * ySpeed * Time.deltaTime) + transform.localPosition.y,-yRange,yRange);
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        float horizontalThrow = Mathf.Clamp((Input.GetAxis("Horizontal") * xSpeed * Time.deltaTime) + transform.localPosition.x, -xRange, xRange);
+        float verticalThrow = Mathf.Clamp((Input.GetAxis("Vertical") * ySpeed * Time.deltaTime) + transform.localPosition.y, -yRange, yRange);
         transform.localPosition = new Vector3(horizontalThrow, verticalThrow, transform.localPosition.z);
-	}
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = (transform.localPosition.y * positionPitchFactor) + (Input.GetAxis("Vertical") * controlPitchFactor * Time.deltaTime);
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = Input.GetAxis("Horizontal") * controlYawFactor;
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Detected With " + other.name);
+    }
 }
